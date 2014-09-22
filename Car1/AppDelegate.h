d4810a45a21bbb5a930247d23c5326ce9e62cea1
apple <apple@appledeMacBook-Pro.local>
2014-09-22 15:56:41 +0800

    Initial Commit

---
+++ Car1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Car1
+//
+//  Created by apple on 14-9-22.
+//  Copyright (c) 2014年 Helen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Car1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Car1
+//
+//  Created by apple on 14-9-22.
+//  Copyright (c) 2014年 Helen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
